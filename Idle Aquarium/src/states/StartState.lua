6db89bcae8624c80b3ab67793f38db7d4c326834
KnickKnaack <nicholasgrijalva01@gmail.com>
2021-02-28 14:49:41 -0700

    Added game framework From Harvard's GD50 on EDX

---
+++ Idle Aquarium/src/states/StartState.lua
@@ -0,0 +1,62 @@
+--[[
+    GD50
+    Breakout Remake
+
+    -- StartState Class --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    Represents the state the game is in when we've just started; should
+    simply display "Breakout" in large text, as well as a message to press
+    Enter to begin.
+]]
+
+-- the "__includes" bit here means we're going to inherit all of the methods
+-- that BaseState has, so it will have empty versions of all StateMachine methods
+-- even if we don't override them ourselves; handy to avoid superfluous code!
+StartState = Class{__includes = BaseState}
+
+function StartState:enter(params)
+end
+
+function StartState:update(dt)
+    -- we no longer have this globally, so include here
+    if love.keyboard.wasPressed('escape') then
+        love.event.quit()
+    end
+end
+
+function StartState:render()
+    -- title
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.setFont(gFonts['large'])
+    love.graphics.printf("Idle Aquarium", 0, VIRTUAL_HEIGHT / 3,
+        VIRTUAL_WIDTH, 'center')
+    
+
+    -- instructions
+    --[[
+    love.graphics.setFont(gFonts['medium'])
+
+    -- if we're highlighting 1, render that option blue
+    if highlighted == 1 then
+        love.graphics.setColor(103/255, 1, 1, 1)
+    end
+    love.graphics.printf("START", 0, VIRTUAL_HEIGHT / 2 + 70,
+        VIRTUAL_WIDTH, 'center')
+
+    -- reset the color
+    love.graphics.setColor(1, 1, 1, 1)
+
+    -- render option 2 blue if we're highlighting that one
+    if highlighted == 2 then
+        love.graphics.setColor(103/255, 1, 1, 1)
+    end
+    love.graphics.printf("HIGH SCORES", 0, VIRTUAL_HEIGHT / 2 + 90,
+        VIRTUAL_WIDTH, 'center')
+
+    -- reset the color
+    love.graphics.setColor(1, 1, 1, 1)
+    ]]
+end
